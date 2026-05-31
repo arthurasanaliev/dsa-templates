96c2af0262902bfbb1f8de7e7a4ccc053c632c51
arthurasanaliev <arthur@io.net>
2026-05-31 18:15:26 +0600

    Add is_prime function

--- number-theory/functions.cpp
+++ number-theory/functions.cpp
@@ -14,3 +14,11 @@ int range_sum(int f, int t) {
     // Returns [f, t] range sum
     return (t - f + 1) * (f + t) / 2;
 }
+
+bool is_prime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
